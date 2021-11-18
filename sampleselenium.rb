2cfc6b40a9b6f84c8e14bf7b2106162c24990b45
vshaltft <94450209+vshaltft@users.noreply.github.com>
2021-11-18 19:08:57 +0530

    Add files via upload

--- sampleselenium.rb
+++ sampleselenium.rb
@@ -2,13 +2,29 @@ require "selenium-webdriver"
 require "rspec"
 
 
-describe "Sample script" do
+describe "SCript to add item in the cart" do
+	before(:all) do
 	driver = Selenium::WebDriver.for :chrome
 	driver.navigate.to "https://www.amazon.in"
 	driver.manage.window.maximize
-it "Search for an item" do
+end 
+context "Search for different laptops" do
+it "Search for an  HP Pavillion Laptop laptop" do
 	sleep 15
-	textbox =driver.find_element(:xpath,"//*[@id='twotabsearchtextbox']")
-	textbox.send_keys("MObiles",:enter)
+	textbox =driver.find_element(:xpath,"//*[@class='nav-search-field ']/input")
+	textbox.send_keys("laptops",:enter)
+	selecthp = driver.find_element(:xpath,"//*[contains(text(),'HP Pavilion (2021)')]").click()
+	sleep 5
+	handles = driver.window_handles
+	childwin = driver.switch_to.window(handles[1])
+	sleep 2
+	addtocartbtn = driver.find_element(:xpath, "//*[@id ='submit.add-to-cart']")
+    driver.execute_script "arguments[0].scrollIntoView();",addtocartbtn
+    addtocartbtn.click() 
+    sleep 5
+    cartelmnt = driver.find_element(:xpath,"//span[@id='attach-sidesheet-view-cart-button-announce']")
+    wait = Selenium::WebDriver::Wait.new(timeout: 30)
+    wait.until { cartelmnt.displayed?}
 end 
 end 
+end
